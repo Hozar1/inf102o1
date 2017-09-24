9beec4c3e1ef9f05c492263b761ded37af7da285
otbho <otbho@DESKTOP-SOVMPE7>
2017-09-24 11:37:53 +0200

    Setting up input of lists
    outerlist is unnecessary

--- src/inf102o1/exactTriplicates.java
+++ src/inf102o1/exactTriplicates.java
@@ -1,5 +1,59 @@
 package inf102o1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class exactTriplicates {
+
+	//Merge all lists
+	//sort the long list
+	//go through each element in list
+	//if the item == pKey
+	//dCount++ --> if dCount ==3 return pKey and break
+	//else set pKey == Item
+	
+	public static ArrayList<ArrayList> findTrip(ArrayList arr){
+		
+		
+		
+		return arr;
+		
+	}
+
+
+
+
+
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		ArrayList<String> list0 = new ArrayList<String>();
+		ArrayList<String> list1 = new ArrayList<String>();
+		ArrayList<String> list2 = new ArrayList<String>();
+		ArrayList<String> list3 = new ArrayList<String>();
+		ArrayList<ArrayList> outerList = new ArrayList<ArrayList>();
+		outerList.add(list0);
+		outerList.add(list1);
+		outerList.add(list2);
+		outerList.add(list3);
+		
+		
+		for(int i=0;i<4;i++){
+					
+			File inFile = new File (System.getProperty("user.dir") + "\\src\\inf102o1\\list"+i+".txt");
+			Scanner sc = new Scanner (inFile);
+			while (sc.hasNextLine())
+			{
+				String line = sc.nextLine();
+				outerList.get(i).add(line);
+			}
+			sc.close();
+		}
+
+
+	}
+
+
 
 }
