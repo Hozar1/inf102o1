0c34fcee2e2f6719c1df672ec0e6463d3fe7cf01
otbho <otbho@DESKTOP-SOVMPE7>
2017-09-25 14:34:06 +0200

    Now correctly counts triples, ignoring if if its >3

--- src/inf102o1/exactTriplicates.java
+++ src/inf102o1/exactTriplicates.java
@@ -3,7 +3,6 @@ package inf102o1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class exactTriplicates {
@@ -18,22 +17,21 @@ public class exactTriplicates {
 	public static String findTrip(ArrayList<String> arr){
 		//Sort
 		arr.sort(null);
-
 		//go through each element in list
 		//if the item == pKey
-		//dCount++ --> if dCount ==3 return pKey and break
+		//dCount++ --> if dCount ==3&& it is either the last element or
+		//the next item is not equals, then return pKey
 		//else set pKey == Item
 		String pKey="";
 		String item="";
 		int dCount =0;
-
 		for(int i=0;i<arr.size();i++)
 		{
 			item =arr.get(i);
 			if(item.equals(pKey))
 			{
 				dCount++;
-				if(dCount == 3)
+				if((dCount == 3)&&(i < arr.size() - 1 ||!arr.get(i+1).equals(pKey)))
 					return pKey;
 			}
 			else
@@ -46,21 +44,22 @@ public class exactTriplicates {
 	}
 
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException{
-
+		//Create empty lists, ready for input. Add them to an outerlist
 		ArrayList<String> list0 = new ArrayList<String>();
 		ArrayList<String> list1 = new ArrayList<String>();
 		ArrayList<String> list2 = new ArrayList<String>();
 		ArrayList<String> list3 = new ArrayList<String>();
+		@SuppressWarnings("rawtypes")
 		ArrayList<ArrayList> outerList = new ArrayList<ArrayList>();
 		outerList.add(list0);
 		outerList.add(list1);
 		outerList.add(list2);
 		outerList.add(list3);
 
-
+		//Read in 4 files from user directory, throwing them into their repsective lists
 		for(int i=0;i<4;i++){
-
 			File inFile = new File (System.getProperty("user.dir") + "\\src\\inf102o1\\list"+i+".txt");
 			Scanner sc = new Scanner (inFile);
 			while (sc.hasNextLine())
@@ -75,9 +74,8 @@ public class exactTriplicates {
 		//merge all lists
 		ArrayList<String> combined = new ArrayList<String>();
 		for(int i=0;i<4;i++)
-			combined.addAll(outerList.get(i));
-		
-		
+			combined.addAll(outerList.get(i));	
+
 		System.out.println(findTrip(combined));
 
 
