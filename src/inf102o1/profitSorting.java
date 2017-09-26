a6f8a43e8dc07a0ef24d1fe6c725634c9902ebc8
Tiwi <Tiwi@MNB-PC>
2017-09-26 12:05:04 +0200

    matten er done

---
+++ src/inf102o1/profitSorting.java
@@ -0,0 +1,57 @@
+package inf102o1;
+import edu.princeton.cs.algs4.StdOut; 
+import edu.princeton.cs.algs4.Stopwatch; 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.BinarySearch;
+import java.math.*;
+import java.util.Arrays;
+
+public class profitSorting{
+	
+    public static int LNsearch(int[] randomList, int Target){
+        
+        int size = randomList.length;
+        for(int i=0;i<size;i++){
+            if(randomList[i] == Target){
+                return i;
+            }
+        }
+        return -1;
+    }
+	
+	public double runBS(int[] randomList,int Target){
+		Stopwatch timer = new Stopwatch();
+		Arrays.sort(randomList);
+		BinarySearch.indexOf(randomList, Target);
+		return timer.elapsedTime();
+	}
+	public double runLN(int[] randomList,int Target){
+		Stopwatch timer = new Stopwatch();
+		LNsearch(randomList, Target);
+		return timer.elapsedTime();
+	}
+	
+	public double test(int Nlength,int Times){
+		int seachTarget = 0;
+		double TimeAlg1 = 0.0;
+		double TimeAlg2 = 0.0;
+		int[] alg1 = new int[Nlength];
+		int[] alg2 = new int[Nlength];
+		for(int t=0; t < Times;t++){
+			for(int k=0;k < Nlength;k++){
+				alg1[k] = StdRandom.uniform((int) 1e6 , (int) 1e7);
+				alg2[k]=alg2[k];
+			}
+			seachTarget = StdRandom.uniform((int) 1e6 , (int) 1e7);
+			TimeAlg1 += runBS(alg1,seachTarget);
+			TimeAlg2 += runLN(alg1,seachTarget);
+		}
+		
+		return TimeAlg1/TimeAlg2;
+	}
+	
+	static public void main(){
+		
+	}
+
+}
