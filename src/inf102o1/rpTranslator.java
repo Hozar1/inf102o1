9fb972c2c4f12e1254744f38c51b8bde69ab589e
Tiwi <Tiwi@MNB-PC>
2017-09-28 13:13:10 +0200

    rewrote for slick reading

--- src/inf102o1/rpTranslator.java
+++ src/inf102o1/rpTranslator.java
@@ -6,57 +6,37 @@ public class rpTranslator {
 
 	public static String rpToInf(String string) {
 		
-		Stack<String> operators = new Stack<String>();
 		Stack<String> shitStack = new Stack<String>();	
 		
 		string.replaceAll("\\s","");
-		String[] splitString = string.split(" ");		
-		String endString = "";
-		String tempString = "";
+		String[] splitString = string.split(" ");
 		for(int i = 0; i<splitString.length ; i++)
 		{
-			String s = splitString[i];			
+			String endString = "";
+			String s = splitString[i];
+			//System.out.println(splitString[i]);
 			switch(s){
+			case "∗" :
+			case "-" :
+			case "/" :	
 			case "*" :
 			case "+" :
-				if(operators.toArray().length > 1){
-					tempString += operators.pop(); 
-					operators.push(s);
-					tempString += operators.pop(); 
-					tempString += operators.pop();
-					tempString = "(" + tempString + ")";
-					shitStack.push(tempString);
-					tempString = "";
-				}
-				else if(operators.toArray().length > 0) 
-				{
-					tempString +=operators.pop();
-					tempString +=s;
-					tempString +=shitStack.pop();
-					tempString = "(" + tempString + ")";
-					shitStack.push(tempString);
-					tempString = "";
-				}
-				else
-				{
 					endString +=shitStack.pop();
 					endString +=s;
 					endString +=shitStack.pop();
 					endString = "(" + endString + ")";
-				}				
+					shitStack.push(endString);	
 				break;
 			default :
-				operators.push(s);			
+				shitStack.push(s);			
 			}			
 		}
-		
-		
-		System.out.println(endString);
+		System.out.println(shitStack.pop());
 		return null;
 	}
 	
 	public static void main(String[] args){
-		  String a = "1 3 + 2 4 2 * + *";
+		  String a = "1 3 + 2 4 2 * + * 4 / 4 5 + +";
 		  rpToInf(a);
 	  
 		}
