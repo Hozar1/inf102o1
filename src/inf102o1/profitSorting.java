4d899ab8de6cbe6d0f56438c15aa03902048a7cf
Tiwi <Tiwi@MNB-PC>
2017-09-28 12:28:38 +0200

    sdf

--- src/inf102o1/profitSorting.java
+++ src/inf102o1/profitSorting.java
@@ -7,55 +7,61 @@ import java.util.Arrays;
 
 public class profitSorting{
 	
-    public static int LNsearch(int[] randomList, int Target){
+	
+    public static int LNsearch(int[] randomList, int target){
         
         int size = randomList.length;
         for(int i=0;i<size;i++){
-            if(randomList[i] == Target){
+            if(randomList[i] == target){
                 return i;
             }
         }
         return -1;
     }
 	
-	public double runBS(int[] randomList,int Target){
-		Stopwatch timer = new Stopwatch();
+	public static long runBS(int[] randomList,int target){
+		long timer = System.currentTimeMillis();
 		Arrays.sort(randomList);
-		BinarySearch.indexOf(randomList, Target);
-		return timer.elapsedTime();
-	}
-	public double runLN(int[] randomList,int Target){
-		Stopwatch timer = new Stopwatch();
-		LNsearch(randomList, Target);
-		return timer.elapsedTime();
+		int BNSearchvar = BinarySearch.indexOf(randomList, target);
+		long savedTimer = System.currentTimeMillis() - timer;
+		StdOut.println("BNSearch time: " + savedTimer + " Result: " + BNSearchvar);
+		return savedTimer;
 	}
 	
-	public double test(int Nlength,int Times){
+	public static long runLN(int[] randomList,int target){
+		long timer = System.currentTimeMillis();
+		int lnSearchvar =LNsearch(randomList, target);
+		long savedTimer = System.currentTimeMillis() - timer;
+		StdOut.println("lnSearch time: " + savedTimer + " Result: " + lnSearchvar);
+		return savedTimer;
+	}
+	
+	public static long test(int nLength,int times){
 		int seachTarget = 0;
-		double TimeAlg1 = 0.0;
-		double TimeAlg2 = 0.0;
-		int[] alg1 = new int[Nlength];
-		int[] alg2 = new int[Nlength];
-		for(int t=0; t < Times;t++){
-			for(int k=0;k < Nlength;k++){
-				alg1[k] = StdRandom.uniform((int) 1e7);
-				alg2[k]=alg2[k];
+		long timeAlg1 = 0L;
+		long timeAlg2 = 0L;
+			int[] alg1 = new int[nLength];
+			int[] alg2 = new int[nLength];
+			for(int k=0;k < nLength;k++){
+				alg1[k] = StdRandom.uniform(1000000000);
+				alg2[k]= alg1[k];
 			}
-			seachTarget = StdRandom.uniform((int) 1e7);
-			TimeAlg1 += runBS(alg1,seachTarget);
-			TimeAlg2 += runLN(alg1,seachTarget);
+			for(int t=0; t < times;t++){ 
+			seachTarget = StdRandom.uniform(1000000000);
+			timeAlg1 += runBS(alg1,seachTarget);
+			timeAlg2 += runLN(alg2,seachTarget);
 		}
-		
-		return TimeAlg1/TimeAlg2;
+		StdOut.println("TimeBN " + timeAlg1 + "\n" + "TimeLN " + timeAlg2);
+		return timeAlg1/timeAlg2;
 	}
 	
-	public void main(){
+	public static void main(String args[]){
 		
-		double Compared =test((int)1e6 ,5);
-		StdOut.println("Binary / Linear = " + Compared);
-		for(int s=10; s<80; s+=10){
-			Compared = test((int)1e6 ,s);
-			StdOut.println("Binary / Linear = " + Compared);
+		long compared =test(1000000,5);
+		StdOut.println("(5)Binary / Linear = " + compared);
+		for(int s=5; s<800; s*=2){
+			compared = test(1000000,s);
+			StdOut.println("("+ s +")"+"Binary / Linear = " + compared);
 		}
 	}
 
